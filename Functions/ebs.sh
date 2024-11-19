34cba3e260a11c4855d6be62c8b77722581837ad
balatuluva <bala.tulva@gmail.com>
2024-11-19 06:56:48 +0000

    Deleting The Volumes which are less than 5GB and not in use only

--- Functions/ebs.sh
+++ Functions/ebs.sh
@@ -1,5 +1,4 @@
 # Deleting The Volumes which are less than 5GB
-
 #!/bin/bash
 delete_vols() {
   vols=$(aws ec2 describe-volumes | jq ".Volumes[].VolumeId" -r)
@@ -13,6 +12,29 @@ delete_vols() {
     fi
   done
 }
-
 #call the function
+delete_vols
+
+# Deleting The Volumes which are less than 5GB and not in use only
+#!/bin/bash
+delete_vols() {
+    # Fetch all volumes
+    vols=$(aws ec2 describe-volumes | jq ".Volumes[].VolumeId" -r)
+    for vol in $vols; do
+        # Fetch volume details
+        volume_info=$(aws ec2 describe-volumes --volume-ids $vol)
+        size=$(echo "$volume_info" | jq ".Volumes[].Size")
+        state=$(echo "$volume_info" | jq ".Volumes[].State" -r)
+        # Check volume size and state
+        if [ "$state" == "in-use" ]; then
+            echo "$vol is attached to an instance. Skipping deletion."
+        elif [ "$size" -gt 5 ]; then
+            echo "$vol is larger than 5GB. Skipping deletion."
+        else
+            echo "Deleting Volume $vol"
+            aws ec2 delete-volume --volume-id $vol
+        fi
+    done
+}
+# Call the function
 delete_vols
